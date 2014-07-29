86659a166a6d98436fe9d9581562099d4b1389f2
casell <supercasell@gmail.com>
2014-07-29 10:28:15 +0200

    Polished structure and packages related files

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 import sys
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 install_requires = ['requests', ]
 
@@ -12,13 +17,24 @@ version = sys.version_info[:2]
 if version < (2, 7) or (3, 0) <= version <= (3, 1):
     install_requires += ['argparse', ]
 
-setup(name='APRSQuake',
+setup(name='aprsquake',
+      version='0.0.1',
+      description='Queries USGS for earthquakes and sends APRS commands',
+      long_description=long_description,
+      url='https://github.com/casell/aprsquake',
+      license='unlicense.org',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'License :: Public Domain',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Communications :: Ham Radio',
+          'Topic :: Scientific/Engineering',
+      ],
       install_requires=install_requires,
-      long_description='Queries USGS for earthquakes and outputs APRS commands',
-      packages=['aprsquake', ],
-      package_data={'': ['LICENSE', 'README.md']},
-      package_dir={'requests': 'requests', },
-      include_package_data=True,
+      keywords='aprs earthquake usgs',
+      packages=find_packages(),
       entry_points={
           'console_scripts': ['aprsquake = aprsquake:main', ],
       })
